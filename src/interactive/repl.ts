7e174cac58219af660344c0a1ff7e2865c4da35e
SixZero <havliktomi@hotmail.com>
2021-10-20 09:40:33 +0200

    Fixed checkpoint of last ran code

--- src/interactive/repl.ts
+++ src/interactive/repl.ts
@@ -42,23 +42,16 @@ function startREPLCommand() {
     startREPL(false, true)
 }
 
-let last_cell_code = null;
 async function executeLastCell() {
 
 
-    if (last_cell_code != null) {
-        const [cellrange, code] = last_cell_code
+    if (last_cell_code !== null) {
+        const [cellrange, ed, doc, code] = last_cell_code
 
-        const ed = vscode.window.activeTextEditor
-        if (ed === undefined) {
-            return
-        }
-        const doc = ed.document
-        // const curcellrange = currentCellRange(ed)
         const curr_code = doc.getText(cellrange)
 
         if (code === curr_code) {
-            const module: string = await modules.getModuleForEditor(ed.document, cellrange.start)
+            const module: string = await modules.getModuleForEditor(doc, cellrange.start)
             await evaluate(ed, cellrange, code, module)
         } else {
             vscode.window.showWarningMessage(
